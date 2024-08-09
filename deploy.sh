3656e56602f49c4b456c4d29001fd39c47963413
zhangwenling <2570104770@qq.com>
2024-08-09 15:33:46 +0800

    fix:fix

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:zwl098/conponents-page.git main:gh-pages
+git push -f git@github.com:zwl098/conponents-page.git main:main
 
 cd -
